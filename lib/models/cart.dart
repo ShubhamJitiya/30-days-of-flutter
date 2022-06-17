348537255cd6404ad1e45fe51d23040666ded8e0
TechTalkerShubh <jitiya66@gmail.com>
2022-06-17 12:38:55 +0530

    day23 completed: writing logic

---
+++ lib/models/cart.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_application_1/models/catalog.dart';
+
+class CartModel {
+  //Catalog field
+  CatalogModel? _catalog;
+
+  //Collection of IDs - store Ids of each item
+  final List<int> _itemIds = [];
+
+  //Get Catalog
+  CatalogModel? get catalog => _catalog;
+
+  set catalog(CatalogModel? newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  //Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog!.getById(id)).toList();
+
+  //Get total price
+  num get totalPrice =>
+      items.fold(0, (totalPrice, current) => totalPrice + current.price);
+
+  //Add Item
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  //Remove item
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
